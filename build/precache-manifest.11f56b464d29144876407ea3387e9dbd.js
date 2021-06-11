3e8e3a56ff4c0c8198fe446c29de536c04590140
Pavan S Patil <psp3075@gmail.com>
2021-06-11 12:51:24 +0530

    readMe update

---
+++ build/precache-manifest.11f56b464d29144876407ea3387e9dbd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b0c51b6ffdeeddf4ebdab62a17f1879",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15583730512ea8bcdd88",
+    "url": "/static/css/2.38ca0958.chunk.css"
+  },
+  {
+    "revision": "d777fd50be2e0fba18bc",
+    "url": "/static/css/main.4e26d8f1.chunk.css"
+  },
+  {
+    "revision": "15583730512ea8bcdd88",
+    "url": "/static/js/2.3c4fd88a.chunk.js"
+  },
+  {
+    "revision": "74abecdc3638247e00d3cef20bdbaf4e",
+    "url": "/static/js/2.3c4fd88a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d777fd50be2e0fba18bc",
+    "url": "/static/js/main.c0474eab.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  }
+]);
